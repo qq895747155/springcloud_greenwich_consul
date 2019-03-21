58be5eef44fb66c410e5d676f0aebe0d01db8604
hdl <handl_bj@163.com>
2019-03-21 19:36:50 +0800

    springcloud zipkin

--- up_server/src/main/java/com/hdl/up_server/UpServerApplication.java
+++ up_server/src/main/java/com/hdl/up_server/UpServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
